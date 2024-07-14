4d488c1fcd8721b77651f6e13e6fa2282284215a
gw <69770151+jinjinjojo@users.noreply.github.com>
2024-07-14 20:41:47 +0000

    update1

--- prox/uv/uv.config.js
+++ prox/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/prox/flamepass-premium/',
-    bare: 'https://g-d.me/',
+    prefix: '/prox/0c2f1ddc-5748-436f-ae7c-89b5f708d90e/',
+    bare: 'https://sellix-key-checker-2sfjgz2adq-uc.a.run.app/0acc41f5-6f26-4480-ac31-3f72706a2fc8/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/prox/uv/uv.handler.js',
